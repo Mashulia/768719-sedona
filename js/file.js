71a7b922e75db973d7071062ff6224f33e91e29e
Mary <mane44ka86@mail.ru>
2018-07-04 11:48:07 +0400

    добавлена валидация формы, сделана минификация и немного правок

--- js/file.js
+++ js/file.js
@@ -1,13 +1,72 @@
-var open = document.querySelector(".modal-close");
+var link = document.querySelector(".modal-close");
 var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal-close");
-var date = popup.querySelector("[name=arrival-date]");
+var arrival = popup.querySelector("[name=arrival-date]");
+var adults = popup.querySelector("[name=adults]");
+var child = popup.querySelector("[name=children]");
+var departure = popup.querySelector("[name=departure-date]");
+var isStorageSupport = true;
+var arrivalStorage = "";
+var departureStorage = "";
+var adultsStorage = "";
+var childStorage = "";
+var search = popup.querySelector(".search");
 
-open.addEventListener("click", function (evt) {
+try {
+  arrivalStorage = localStorage.getItem("select-arrival")
+  departureStorage = localStorage.getItem("select-departure")
+  adultsStorage = localStorage.getItem("select-adults")
+  childStorage = localStorage.getItem("select-child")
+  }
+  catch (err) {
+    isStorageSupport = false;
+  }
+
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
-  date.focus();
+  arrival.focus();
+  arrival.classList.remove("modal-error");
+  departure.classList.remove("modal-error");
+  adults.classList.remove("modal-error");
+  child.classList.remove("modal-error");
 });
+
+popup.addEventListener("submit", function (evt) {
+  if (!arrival.value)  {
+    evt.preventDefault();
+    console.log("Нужно ввести дату прибытия");
+    arrival.classList.remove("modal-error");
+    arrival.offsetWidth = arrival.offsetWidth;
+    arrival.classList.add("modal-error");
+  }
+  if (!departure.value)  {
+    evt.preventDefault();
+    console.log("Нужно ввести дату отъезда");
+    departure.classList.remove("modal-error");
+    departure.offsetWidth = departure.offsetWidth;
+    departure.classList.add("modal-error");
+  }
+  if (!adults.value) {
+    adults.classList.remove("modal-error");
+    console.log("Нужно ввести количество взрослых");
+    adults.offsetWidth = adults.offsetWidth;
+    adults.classList.add("modal-error");
+  }
+  if (!child.value) {
+    child.classList.remove("modal-error");
+    console.log("Нужно ввести количесто детей");
+    child.offsetWidth = child.offsetWidth;
+    child.classList.add("modal-error");
+  }
+   else {
+      if (isStorageSupport) {
+      localStorage.setItem("select-aarrival", arrival.value);
+      localStorage.setItem("select-departure", departure.value);
+      localStorage.setItem("select-adults", adults.value);
+      localStorage.setItem("select-child", child.value);
+     }
+   }
+  });
 
 document.documentElement
         .classList.replace('no-js', 'js');
