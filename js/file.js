0052476c10be4e1362498ebbe94dd0d2535c19a0
Mary <mane44ka86@mail.ru>
2018-07-05 11:29:37 +0400

    поправила код, выправила по пиксельперфект

--- js/file.js
+++ js/file.js
@@ -12,61 +12,59 @@ var childStorage = "";
 var search = popup.querySelector(".search");
 
 try {
-  arrivalStorage = localStorage.getItem("select-arrival")
-  departureStorage = localStorage.getItem("select-departure")
-  adultsStorage = localStorage.getItem("select-adults")
-  childStorage = localStorage.getItem("select-child")
-  }
-  catch (err) {
+    arrivalStorage = localStorage.getItem("select-arrival")
+    departureStorage = localStorage.getItem("select-departure")
+    adultsStorage = localStorage.getItem("select-adults")
+    childStorage = localStorage.getItem("select-child")
+} catch (err) {
     isStorageSupport = false;
-  }
+}
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("modal-show");
-  arrival.focus();
-  arrival.classList.remove("modal-error");
-  departure.classList.remove("modal-error");
-  adults.classList.remove("modal-error");
-  child.classList.remove("modal-error");
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+    arrival.focus();
+    arrival.classList.remove("modal-error");
+    departure.classList.remove("modal-error");
+    adults.classList.remove("modal-error");
+    child.classList.remove("modal-error");
 });
 
-popup.addEventListener("submit", function (evt) {
-  if (!arrival.value)  {
-    evt.preventDefault();
-    console.log("Нужно ввести дату прибытия");
-    arrival.classList.remove("modal-error");
-    arrival.offsetWidth = arrival.offsetWidth;
-    arrival.classList.add("modal-error");
-  }
-  if (!departure.value)  {
-    evt.preventDefault();
-    console.log("Нужно ввести дату отъезда");
-    departure.classList.remove("modal-error");
-    departure.offsetWidth = departure.offsetWidth;
-    departure.classList.add("modal-error");
-  }
-  if (!adults.value) {
-    adults.classList.remove("modal-error");
-    console.log("Нужно ввести количество взрослых");
-    adults.offsetWidth = adults.offsetWidth;
-    adults.classList.add("modal-error");
-  }
-  if (!child.value) {
-    child.classList.remove("modal-error");
-    console.log("Нужно ввести количесто детей");
-    child.offsetWidth = child.offsetWidth;
-    child.classList.add("modal-error");
-  }
-   else {
-      if (isStorageSupport) {
-      localStorage.setItem("select-aarrival", arrival.value);
-      localStorage.setItem("select-departure", departure.value);
-      localStorage.setItem("select-adults", adults.value);
-      localStorage.setItem("select-child", child.value);
-     }
-   }
-  });
+popup.addEventListener("submit", function(evt) {
+    if (!arrival.value) {
+        evt.preventDefault();
+        console.log("Нужно ввести дату прибытия");
+        arrival.classList.remove("modal-error");
+        arrival.offsetWidth = arrival.offsetWidth;
+        arrival.classList.add("modal-error");
+    }
+    if (!departure.value) {
+        evt.preventDefault();
+        console.log("Нужно ввести дату отъезда");
+        departure.classList.remove("modal-error");
+        departure.offsetWidth = departure.offsetWidth;
+        departure.classList.add("modal-error");
+    }
+    if (!adults.value) {
+        adults.classList.remove("modal-error");
+        console.log("Нужно ввести количество взрослых");
+        adults.offsetWidth = adults.offsetWidth;
+        adults.classList.add("modal-error");
+    }
+    if (!child.value) {
+        child.classList.remove("modal-error");
+        console.log("Нужно ввести количесто детей");
+        child.offsetWidth = child.offsetWidth;
+        child.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("select-aarrival", arrival.value);
+            localStorage.setItem("select-departure", departure.value);
+            localStorage.setItem("select-adults", adults.value);
+            localStorage.setItem("select-child", child.value);
+        }
+    }
+});
 
 document.documentElement
-        .classList.replace('no-js', 'js');
+    .classList.replace('no-js', 'js');
